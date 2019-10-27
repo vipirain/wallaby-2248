5ffcef96493220d26dad352a5916675f6742a2a1
vipirain <vito.piraino@gmail.com>
2019-10-28 00:44:59 +0100

    Add angular material
    This breaks the test

---
+++ app.component.spec.ts
@@ -0,0 +1,40 @@
+import { By } from '@angular/platform-browser';
+import { TestBed, async } from '@angular/core/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { MaterialModule } from './material.module';
+
+describe('AppComponent', () => {
+
+  let fixture;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        MaterialModule,
+        ReactiveFormsModule,
+        BrowserAnimationsModule
+      ],
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+  }));
+
+  it('correct dateStart should be valid', () => {
+    console.log(navigator.userAgent);
+    const inputDateStart = fixture.debugElement.query(By.css('[formControlName=dateStart]')).nativeElement;
+    inputDateStart.value = 'valid';
+    inputDateStart.dispatchEvent(new Event('input'));
+    inputDateStart.dispatchEvent(new Event('change')); // redundant event, can be removed
+    fixture.detectChanges();
+    expect(inputDateStart.classList.contains('ng-invalid')).toBeFalsy('dateStart should be valid');
+  });
+
+});
---
+++ app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MaterialModule } from './material.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    MaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
